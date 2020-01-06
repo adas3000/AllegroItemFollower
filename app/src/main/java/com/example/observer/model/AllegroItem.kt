6bf4c89e767987c295358e990bc8223b64ca7987
Adam_Z <youngtoss38@gmail.com>
2020-01-06 13:17:36 +0100

    allegro item turned into entity(room added to gradle dep.)

--- app/src/main/java/com/example/observer/model/AllegroItem.kt
+++ app/src/main/java/com/example/observer/model/AllegroItem.kt
@@ -1,12 +1,26 @@
 package com.example.observer.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 class AllegroItem {
 
+    @PrimaryKey
+    val uid:Int
+    
+    @ColumnInfo(name = "item_name")
     var itemName:String
+
+    @ColumnInfo(name="price")
     var itemPrice:Int
+
+    @ColumnInfo(name="url")
     var itemURL:String
 
-    constructor(itemName: String, itemPrice: Int, itemURL: String) {
+    constructor(uid: Int, itemName: String, itemPrice: Int, itemURL: String) {
+        this.uid = uid
         this.itemName = itemName
         this.itemPrice = itemPrice
         this.itemURL = itemURL
