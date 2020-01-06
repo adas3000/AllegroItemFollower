a1ea51fb1b0f6788e7c6c95a17b32e67f69204f3
Adam_Z <youngtoss38@gmail.com>
2020-01-06 12:43:34 +0100

    MainActivity update

--- app/src/main/java/com/example/observer/MainActivity.kt
+++ app/src/main/java/com/example/observer/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.observer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.observer.fragment.ItemListFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,9 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val supportFragmentManager :FragmentManager= supportFragmentManager
+        val fragmentTransition :FragmentTransaction= supportFragmentManager.beginTransaction()
 
-
-
+        fragmentTransition.add(R.id.frameLayout_fragmentKeeper,ItemListFragment())
+        fragmentTransition.commit()
     }
 
 
