19356569d7cdfe40e62dc195502074d6f91ebc1a
Adam_Z <youngtoss38@gmail.com>
2020-01-07 13:48:50 +0100

    getObserver method added in JsoupUrlPresenter

--- app/src/main/java/com/example/observer/presenter/JsoupUrlPresenter.kt
+++ app/src/main/java/com/example/observer/presenter/JsoupUrlPresenter.kt
@@ -1,7 +1,12 @@
 package com.example.observer.presenter
 
+import android.util.Log
 import com.example.observer.view.IJsoupUrlView
 import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import java.io.IOException
@@ -9,15 +14,15 @@ import java.lang.RuntimeException
 
 class JsoupUrlPresenter(jsoupurlView: IJsoupUrlView) : IJsoupUrlPresenter {
 
+    private val TAG = "JsoupUrlPresenter"
 
-    private fun getJsoupProx(url:String):Observable<Document>{
+    private fun getJsoupProx(url: String): Observable<Document> {
 
-        return Observable.fromCallable<Document>{
-            try{
+        return Observable.fromCallable<Document> {
+            try {
                 val doc = Jsoup.connect(url).get()
                 return@fromCallable doc
-            }
-            catch(e:IOException){
+            } catch (e: IOException) {
                 throw RuntimeException(e)
             }
         }
@@ -26,10 +31,34 @@ class JsoupUrlPresenter(jsoupurlView: IJsoupUrlView) : IJsoupUrlPresenter {
 
     override fun scanURL(url: String) {
 
-        
+        val observer: Observer<Document> = getObserver()
+
+        getJsoupProx(url)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(observer)
 
 
     }
 
+    private fun getObserver(): Observer<Document> {
+        return object : Observer<Document> {
+            override fun onComplete() {
+                Log.d(TAG, "onComplete invoked")
+            }
+
+            override fun onNext(t: Document) {
+                Log.d(TAG, "onNext invoked")
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG, "onSubscribed invoked")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG, "onError invoked")
+            }
+        }
+    }
 
 }
