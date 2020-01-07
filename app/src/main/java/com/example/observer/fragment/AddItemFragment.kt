14f3928dd067563c0fddbd6a169018d6a7f1beb4
Adam_Z <youngtoss38@gmail.com>
2020-01-07 15:34:27 +0100

    onGetAllItems implemented

--- app/src/main/java/com/example/observer/fragment/AddItemFragment.kt
+++ app/src/main/java/com/example/observer/fragment/AddItemFragment.kt
@@ -109,7 +109,6 @@ class AddItemFragment : Fragment() , IAddItemView {
                 }
             })
 
-
     }
 
 }
--- app/src/main/java/com/example/observer/presenter/ItemListPresenter.kt
+++ app/src/main/java/com/example/observer/presenter/ItemListPresenter.kt
@@ -1,22 +1,58 @@
 package com.example.observer.presenter
 
+import android.util.Log
 import com.example.observer.db.AppDatabase
+import com.example.observer.model.AllegroItem
 import com.example.observer.view.ItemListView
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class ItemListPresenter : IItemListPresenter {
 
-    val itemListView:ItemListView
+    private val TAG="ItemListPresenter"
 
+    val itemListView:ItemListView
+    lateinit var allegroItemList:List<AllegroItem>
 
     constructor(itemListView:ItemListView){
         this.itemListView = itemListView
     }
 
     override fun onGetAllItems(db: AppDatabase) {
-
-        
-
-
+        val xd =
+        db.allegroItemDao().getAll()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(getObserver())
     }
+
+    fun getObserver():Observer<List<AllegroItem>>{
+        return object:Observer<List<AllegroItem>>{
+
+            override fun onComplete() {
+                Log.d(TAG, "onComplete invoked")
+                itemListView.onFinish(allegroItemList)
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d(TAG,"on error invoked")
+                Log.d(TAG,e.message)
+            }
+
+            override fun onNext(t: List<AllegroItem>) {
+                allegroItemList = t
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d(TAG,"on subscribe invoked")
+            }
+
+        }
+    }
+
+
 
 }
