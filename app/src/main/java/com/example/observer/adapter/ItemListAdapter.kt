015ed714665d3d2c4b3ec449e37b6a8f922db886
Adam_Z <youngtoss38@gmail.com>
2020-01-07 15:11:55 +0100

    itemListAdapter(recyclerView) created

---
+++ app/src/main/java/com/example/observer/adapter/ItemListAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.observer.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.observer.R
+import com.example.observer.model.AllegroItem
+
+class ItemListAdapter(val itemList:List<AllegroItem>):RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.item_name.setText(itemList[position].itemName)
+        holder.price.setText(itemList[position].itemPrice.toString())
+        holder.url.setText(itemList[position].itemURL)
+        
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.itemlist_fragment_layout,parent,false)
+        return ViewHolder(v)
+    }
+
+
+
+    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val url = view.findViewById<TextView>(R.id.item_data_layout_textView_url)
+        val item_name = view.findViewById<TextView>(R.id.item_data_layout_textView_title)
+        val price = view.findViewById<TextView>(R.id.item_data_layout_textView_price)
+    }
+
+
+}
