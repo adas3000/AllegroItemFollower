24da3ca8b26cd226ea353eb3e3107fa80be98356
Adam_Z <youngtoss38@gmail.com>
2020-01-07 17:47:34 +0100

    monitoring internet access added

--- app/src/main/java/com/example/observer/MainActivity.kt
+++ app/src/main/java/com/example/observer/MainActivity.kt
@@ -7,27 +7,62 @@ import androidx.fragment.app.FragmentTransaction
 import com.example.observer.fragment.AddItemFragment
 import com.example.observer.fragment.ItemListFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.net.Network
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG="MainActivity"
+    private val TAG = "MainActivity"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val supportFragmentManager :FragmentManager= supportFragmentManager
-        val fragmentTransition :FragmentTransaction= supportFragmentManager.beginTransaction()
+        var networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onLost(network: Network?) {
+                Log.d(TAG, "onLost invoked")
+            }
 
-        fragmentTransition.add(R.id.frameLayout_fragmentKeeper,ItemListFragment())
+            override fun onUnavailable() {
+                Log.d(TAG, "onUnavailable invoked")
+            }
+
+            override fun onLosing(network: Network?, maxMsToLive: Int) {
+                Log.d(TAG, "onLosing invoked")
+            }
+
+            override fun onAvailable(network: Network?) {
+                Log.d(TAG, "onAvailable invoked")
+
+            }
+        }
+        val connectivityManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkRequest = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+
+
+
+        val supportFragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransition: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+        fragmentTransition.add(R.id.frameLayout_fragmentKeeper, ItemListFragment())
         fragmentTransition.commit()
 
         floatingActionButton3.setOnClickListener {
             val fab_fragmentTransition = supportFragmentManager.beginTransaction()
             floatingActionButton3.hide()
-            fab_fragmentTransition.replace(R.id.frameLayout_fragmentKeeper,AddItemFragment())
+            fab_fragmentTransition.replace(R.id.frameLayout_fragmentKeeper, AddItemFragment())
             fab_fragmentTransition.addToBackStack(null)
             fab_fragmentTransition.commit()
         }
@@ -37,11 +72,10 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         val count = supportFragmentManager.backStackEntryCount
 
-        if(count==0){
+        if (count == 0) {
             super.onBackPressed()
-        }
-        else{
-            if(floatingActionButton3.isOrWillBeHidden) floatingActionButton3.show()
+        } else {
+            if (floatingActionButton3.isOrWillBeHidden) floatingActionButton3.show()
 
             supportFragmentManager.popBackStack()
         }
